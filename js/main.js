91eec298b5fb229a4f860881d7898f2702ea2564
Anghelo Gabriel <anghelogfloresp@gmail.com>
2020-03-12 19:21:28 -0500

    Modificacion de seccion uros

--- js/main.js
+++ js/main.js
@@ -74,12 +74,12 @@
     $("#mobile-nav, #mobile-nav-toggle").hide();
   }
 
-  // Header scroll class
+ // Header scroll class
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
       $('#header').addClass('header-scrolled');
     } else {
-      $('#header').removeClass('header-scrolled');
+      $('.hola').removeClass('header-scrolled');
     }
   });
 
